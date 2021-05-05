12f7d3c7cf28a3e2e600ced1ffa253e42045ca56
arjun391c <arjun391c@gmail.com>
2021-05-05 18:51:42 +0530

    statefulwidget

--- lib/main.dart
+++ lib/main.dart
@@ -5,47 +5,103 @@ void main() => runApp(MaterialApp(
       theme: ThemeData(primarySwatch: Colors.purple),
     ));
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var text = "_";
+
+  TextEditingController _nameController = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Awesome"),
       ),
-      body: Container(
-        color: Colors.black,
-        height: 500,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              // color: Colors.deepOrangeAccent,
-              width: 100,
-              height: 100,
-              // clipBehavior: Clip.antiAlias,
-              padding: const EdgeInsets.all(10),
-              // alignment: Alignment.center,
-              color: Colors.red,
+      backgroundColor: Colors.grey[300],
+      body: Center(
+          child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Card(
+            child: Column(
+              children: <Widget>[
+                Image.asset(
+                  "assets/ninjaParty.jpg",
+                  width: 400,
+                  height: 400,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'Welcome, $text to ninja party!!',
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.5),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    controller: _nameController,
+                    decoration: InputDecoration(
+                        hintText: "Write Something!",
+                        labelText: "Name",
+                        border: OutlineInputBorder()),
+                  ),
+                )
+              ],
             ),
-            Container(
-              // color: Colors.deepOrangeAccent,
-              width: 100,
-              height: 100,
-              // clipBehavior: Clip.antiAlias,
-              padding: const EdgeInsets.all(10),
-              // alignment: Alignment.center,
-              color: Colors.green,
+          ),
+        ),
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          text = _nameController.text;
+          setState(() {});
+        },
+        child: Icon(Icons.edit),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: const EdgeInsets.all(0),
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text("Arjun"),
+              accountEmail: Text("arjun@mail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://lh3.googleusercontent.com/proxy/kYAyBsDbH34vmHK4EWat1vYj53fEA2WzMnCohqC9KBFWaN-njkx33E4AjL8xtWPxklAIMI5COxk3xTJCCCTlHrWrFPe0lIyZ73nJ4tmFVNCS9g4_2DNcYUYgN3VkRTp7vXvI"),
+              ),
             ),
-            Container(
-              // color: Colors.deepOrangeAccent,
-              width: 100,
-              height: 100,
-              // clipBehavior: Clip.antiAlias,
-              padding: const EdgeInsets.all(10),
-              // alignment: Alignment.center,
-              color: Colors.blue,
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Arjun C"),
+              subtitle: Text("Hi i'm a developer."),
+              trailing: Icon(Icons.edit),
             ),
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text("Email"),
+              subtitle: Text("arjun@gmail.com"),
+              trailing: Icon(Icons.edit),
+            )
           ],
         ),
       ),
